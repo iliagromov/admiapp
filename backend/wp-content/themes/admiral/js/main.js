b77da91fb202450ee48de957a1370bc19681c9a8
iliagromov <ilia.workproffile@gmail.com>
2022-08-15 13:12:30 +0300

    added blocks

--- backend/wp-content/themes/admiral/js/main.js
+++ backend/wp-content/themes/admiral/js/main.js
@@ -1,1 +1,17 @@
-let l;
+const swiperSliderBanner = new Swiper('.js--slider-banner', {
+  spaceBetween: 0,
+  slidesPerView: 1,
+  loop: false,
+  navigation: {
+      nextEl: '.js--swiper-button-next',
+      prevEl: '.js--swiper-button-prev',
+  },
+  effect: 'fade',
+  pagination: {
+    el: '.js--swiper-pagination',
+    type: 'bullets',
+  },
+  fadeEffect: {
+    crossFade: true
+  },
+});
