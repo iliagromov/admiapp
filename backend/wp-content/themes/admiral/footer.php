700015504d71990c0d0fa5312c83ba4add5d81cf
iliagromov <ilia.workproffile@gmail.com>
2022-08-18 11:29:09 +0300

    added blocks

--- backend/wp-content/themes/admiral/footer.php
+++ backend/wp-content/themes/admiral/footer.php
@@ -1,5 +1,79 @@
 
-<?php wp_footer(); ?>
+
+<footer class="footer">
+    <div class="footer__nav">
+      <div class="wrapper">
+        <div class="footer-inner">
+          <div class="footer__col">
+            <div class="page__title page_regular page__title-h4">Проживание</div><a class="page-link" href="/">Коттеджи
+              и номера</a><a class="page-link" href="/">Забронировать</a><a class="page-link" href="/">Клубная карта</a>
+          </div>
+          <div class="footer__col">
+            <div class="page__title page_regular page__title-h4">Бани и SPA</div><a class="page-link" href="/">SPA
+              Программы</a>
+          </div>
+          <div class="footer__col">
+            <div class="page__title page_regular page__title-h4">Кухня</div><a class="page-link" href="/">О
+              ресторане</a><a class="page-link" href="/">Меню</a><a class="page-link" href="/">Полный панисион</a><a
+              class="page-link" href="/">Винная карта</a>
+          </div>
+          <div class="footer__col">
+            <div class="page__title page_regular page__title-h4">Отдых и Впечатления</div><a class="page-link"
+              href="/">Ресторан</a><a class="page-link" href="/">Летние впечатления</a><a class="page-link"
+              href="/">Зимние впечатления</a><a class="page-link" href="/">Расслабляющие процедуры</a>
+          </div>
+          <div class="footer__col">
+            <div class="page__title page_regular page__title-h4">Информация</div><a class="page-link" href="/">Правила
+              посещения яхт-клуба</a><a class="page-link" href="/">Реквизиты</a><a class="page-link" href="/">Политика
+              конфиденциальности</a><a class="page-link" href="/">Публичная оферта</a>
+          </div>
+          <div class="footer__col footer__contacts">
+            <div class="page__title page_regular page__title-h4">Контакты</div>
+            <div class="page-fl-aic">
+              <div class="svg-phone svg-phone-box"></div><a class="page-link" href="tel:+7 (495) 222-02-23">+7 (495)
+                222-02-23</a>
+            </div>
+            <div class="page-fl-aic">
+              <div class="svg-mail-solid svg-mail-solid-box"></div><a class="page-link"
+                href="mailto:event@club-admiral.com">event@club-admiral.com</a>
+            </div>
+            <div class="page-fl-aic">Россия, Московская обл., Мытищинский р-н., Виноградовский СО, д. Грибки,</div>
+            <div class="page-fl-aic">ул. Адмиральская, вл. 1, стр.1</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer__bottom">
+      <div class="wrapper">
+        <div class="footer-inner">
+          <div class="footer__logo"><a href="/"><img src="assets/images/svg/logo.svg" alt="logo"></a>
+            <div class="footer__copy">© 2022 Jura Software </div>
+          </div>
+          <div class="footer__social">
+            <div class="select-city">
+              <div class="svg-globe-solid svg-globe-solid-box"></div>
+              <p class="page__text">Русский (RU)</p>
+            </div>
+            <div class="social"><a class="social-link" href="">
+                <div class="svg-watsapp svg-watsapp-box"></div>
+              </a><a class="social-link" href="">
+                <div class="svg-telegramm svg-vk svg-box"></div>
+              </a><a class="social-link" href="">
+                <div class="svg-telegramm svg-telegramm-box"></div>
+              </a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <link rel="stylesheet" href="css/styles-calc.css" type="text/css">
+  <link rel="stylesheet" href="css/styles-sprites.css" type="text/css">
+  <script src="libs/js/_all/swiper-bundle.min.js"></script>
+  <script src="libs/js/_all/store-scroll.js"></script>
+  <script src="js/main.js"></script>
+  <script src="/js/debuggrid/debuggrid.component.js"></script>
+  <script src="/js/header/header.component.js"></script>
+	<?php wp_footer(); ?>
 </body>
 
 </html>
--- backend/wp-content/themes/admiral/header.php
+++ backend/wp-content/themes/admiral/header.php
@@ -1,10 +1,45 @@
-<!DOCTYPE HTML>
-<html <?php language_attributes(); ?>>
+<!DOCTYPE html>
+<html lang="ru" data-scroll="0">
+
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<?php wp_head(); ?>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="tg@gromov_ilia">
+  <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
+  <base href="<?php echo get_template_directory_uri(); ?>/">
+  <title>Title Pages</title>
+  <link rel="stylesheet" href="libs/css/swiper-bundle.min.css" type="text/css">
+  <link rel="stylesheet" href="css/base-styles.css" type="text/css">
 </head>
 
 <body <?php body_class(); ?>>
+  <!-- FIXME: add variables class-->
+  <!-- <button class="debugGrid-btn debugGrid-btn__create"
+    onclick="debugGridCreate()"></button><button class="debugGrid-btn debugGrid-btn__remove debugGrid-btn__hidden"
+    onclick="debugGridRemove()"></button> -->
+  <div class="debugGrid debugGrid__z-index-negative"></div>
+  <header class="header">
+    <div class="wrapper">
+      <div class="header-inner">
+        <div class="header__logo"><a href="/"><img src="assets/images/svg/logo.svg" alt="logo"></a></div>
+        <div class="header__nav">
+          <nav class="page-nav"> <a class="page-link" href="/">Коттеджи</a><a class="page-link" href="/">Программы</a><a
+              class="page-link" href="/">Кухня</a><a class="page-link" href="/">Впечатления</a></nav>
+        </div>
+        <div class="header__phone">
+          <div class="page-fl-aic">
+            <div class="svg-phone svg-phone-box"></div><a class="page-link" href="tel:+7 (495) 222-02-23">+7 (495)
+              222-02-23</a>
+          </div>
+        </div>
+        <div class="header__menu">
+          <div class="menu">
+            <div class="menu-btn">
+              <div class="svg-view-list svg-view-list-box"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
