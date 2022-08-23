30a7379c459ada7892732b7de81c38ad0c09ccc5
iliagromov <ilia.workproffile@gmail.com>
2022-08-23 11:57:40 +0300

    added cuisine

---
+++ backend/wp-content/themes/admiral/templates/pageCauisine.php
@@ -0,0 +1,194 @@
+<?php
+/*
+	Template Name: Ресторан
+*/
+get_header();
+?>
+<main class="page">
+  <section class="banner banner-cuisine">
+    <div class="banner_bg">
+      <div class="banner_bg-shadow"></div><img src="assets/images/png/cuisine/img-banner-cuisine.png" alt="banner">
+    </div>
+    <div class="wrapper">
+      <div class="banner-inner banner-inner_center">
+        <div class="banner__title_left">
+          <h1 class="page__title page_regular page__title-h1">Ресторан авторской кухни и здоровой еды</h1>
+          <p class="page__text">На территории банного комплекса есть сервис доставки блюд из ресторана с сервировкой в
+            коттедже. </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="cuisine-advantages">
+    <div class="wrapper">
+      <div class="cuisine-advantages-inner">
+        <div class="cuisine-advantages__item">
+          <div class="cuisine-advantages__item_bg"><img src="assets/images/svg/cuisine-advantages-img-1.svg" alt="">
+          </div>
+          <div class="cuisine-advantages__item-text">
+            <h3 class="page__title page_regular page__title-h3">Ежедневный сервис</h3>
+            <p class="page__text">Принимаем заказы каждый день <br> с 10.00 до 23.00</p>
+          </div>
+        </div>
+        <div class="cuisine-advantages__item">
+          <div class="cuisine-advantages__item_bg"><img src="assets/images/svg/cuisine-advantages-img-2.svg" alt="">
+          </div>
+          <div class="cuisine-advantages__item-text">
+            <h3 class="page__title page_regular page__title-h3">Доставка ресторанных блюд</h3>
+            <p class="page__text">Доставим к назначенному часу <br> в удобное для вас время</p>
+          </div>
+        </div>
+        <div class="cuisine-advantages__item">
+          <div class="cuisine-advantages__item_bg"><img src="assets/images/svg/cuisine-advantages-img-3.svg" alt="">
+          </div>
+          <div class="cuisine-advantages__item-text">
+            <h3 class="page__title page_regular page__title-h3">Сервировка и обслуживание</h3>
+            <p class="page__text">Подадим к вашему столу <br> в горячем и эстетичном виде</p>
+          </div>
+        </div>
+        <div class="cuisine-advantages__item">
+          <div class="cuisine-advantages__item_bg"><img src="assets/images/svg/cuisine-advantages-img-4.svg" alt="">
+          </div>
+          <div class="cuisine-advantages__item-text">
+            <h3 class="page__title page_regular page__title-h3">Удобная оплата заказа</h3>
+            <ul class="page-ul">
+              <li class="page-li">Наличными или картой курьеру</li>
+              <li class="page-li">Онлайн-оплата</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="cuisine-shef">
+    <div class="wrapper">
+      <div class="cuisine-shef-inner">
+        <div class="cuisine-shef-item"><img src="assets/images/png/cuisine/img-shef.png" alt="shef"></div>
+        <div class="cuisine-shef-item cuisine-shef-item__text">
+          <h3 class="page__title page_regular page__title-h3 page_braun">Шеф</h3>
+          <h3 class="page__title page_regular page__title-h3">Величев Николай</h3>
+          <p class="page__text page_regular">Признанный мастер в мире кулинарии. Создавал, открывал и консультировал
+            гастрономические заведения по всему миру. Всю свою жизнь посвятил изучению тонкостей этого искусства: опыт
+            работы в сфере более 20 лет.</p>
+          <p class="page__text page_regular">Специалист по приготовлению дикого мяса. В совершенстве владеет навыками приготовления
+            всевозможных настоек, в том числе и медовухи и хреновухи.</p><img src="assets/images/svg/img-shef-rospis.svg" alt="shef">
+        </div>
+        <div class="cuisine-shef-item"><img src="assets/images/png/cuisine/img-shef-2.png" alt="shef"></div>
+      </div>
+    </div>
+  </section>
+  <section class="cuisine-menu">
+    <div class="wrapper">
+      <h3 class="page__title page_regular page__title-h3">Меню ресторана</h3>
+      <div class="cuisine-menu-inner"><a class="cuisine-menu__item" href="">
+          <div class="cuisine-menu__item-img"><img src="assets/images/png/cuisine/menu-img-1.png" alt="menu"></div>
+          <div class="cuisine-menu__item-text">
+            <h4 class="page__title page_regular page__title-h4">Завтраки</h4>
+          </div>
+        </a><a class="cuisine-menu__item" href="">
+          <div class="cuisine-menu__item-img"><img src="assets/images/png/cuisine/menu-img-2.png" alt="menu"></div>
+          <div class="cuisine-menu__item-text">
+            <h4 class="page__title page_regular page__title-h4">Основное меню</h4>
+          </div>
+        </a><a class="cuisine-menu__item" href="">
+          <div class="cuisine-menu__item-img"><img src="assets/images/png/cuisine/menu-img-3.png" alt="menu"></div>
+          <div class="cuisine-menu__item-text">
+            <h4 class="page__title page_regular page__title-h4">Сеты на компанию</h4>
+          </div>
+        </a><a class="cuisine-menu__item" href="">
+          <div class="cuisine-menu__item-img"><img src="assets/images/png/cuisine/menu-img-4.png" alt="menu"></div>
+          <div class="cuisine-menu__item-text">
+            <h4 class="page__title page_regular page__title-h4">От шефа</h4>
+          </div>
+        </a><a class="cuisine-menu__item" href="">
+          <div class="cuisine-menu__item-img"><img src="assets/images/png/cuisine/menu-img-5.png" alt="menu"></div>
+          <div class="cuisine-menu__item-text">
+            <h4 class="page__title page_regular page__title-h4">Десерты</h4>
+          </div>
+        </a><a class="cuisine-menu__item" href="">
+          <div class="cuisine-menu__item-img"><img src="assets/images/png/cuisine/menu-img-6.png" alt="menu"></div>
+          <div class="cuisine-menu__item-text">
+            <h4 class="page__title page_regular page__title-h4">Напитки</h4>
+          </div>
+        </a></div>
+    </div>
+  </section>
+  <section class="cuisine-contacts">
+    <div class="wrapper">
+      <div class="cuisine-contacts-inner">
+        <div class="cuisine-contacts__map"><img src="assets/images/png/cuisine/img-map.png" alt="map"></div>
+        <div class="cuisine-contacts__text">
+          <h3 class="page__title page_regular page__title-h3">Рестораны на территории</h3>
+          <p class="page__text">В шаговой доступности от банного комплексаесть два ресторана:</p>
+          <div class="cuisine-contacts__text-item"><span>«Дикий Койот»: <a class="page-link" href="tel:+7 (977) 697-82-49"> +7 (977) 697-82-49</a></span>
+            <div class="page__text">Пн-Суб: 11.00-21.00, Вс: 11.00-20.00</div>
+          </div>
+          <div class="cuisine-contacts__text-item"><span>«YACHTLOUNGE CAFE BAR»: <a class="page-link" href="tel:+7 (977) 697-54-45"> +7 (977) 697-54-45</a></span>
+            <div class="page__text">Пн-Суб: 11.00-21.00, Вс: 11.00-20.00</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="slider-banner js--slider-banner swiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide slider-banner-slide">
+        <div class="slider-banner-slide__text">
+          <div class="wrapper">
+            <div class="slider-banner-slide-text">
+              <div class="page__title page_regular page__title-h2">Попробуйте программу «SPA VIP PLUS»</div>
+            </div>
+          </div>
+        </div>
+        <div class="slider-banner-slide__img">
+          <div class="slider-banner-slide__img_bg"></div><img src="assets/images/png/main-page/banner-menu.png" alt="banner-spa">
+        </div>
+      </div>
+      <div class="swiper-slide slider-banner-slide">
+        <div class="slider-banner-slide__text">
+          <div class="wrapper">
+            <div class="slider-banner-slide-text">
+              <div class="page__title page_regular page__title-h2"> Попробуйте программу «SPA VIP PLUS»</div>
+            </div>
+          </div>
+        </div>
+        <div class="slider-banner-slide__img">
+          <div class="slider-banner-slide__img_bg"></div><img src="assets/images/png/main-page/banner-spa.png" alt="banner-spa">
+        </div>
+      </div>
+      <div class="swiper-slide slider-banner-slide">
+        <div class="slider-banner-slide__text">
+          <div class="wrapper">
+            <div class="slider-banner-slide-text">
+              <div class="page__title page_regular page__title-h2"> Попробуйте программу «SPA VIP PLUS»</div>
+            </div>
+          </div>
+        </div>
+        <div class="slider-banner-slide__img">
+          <div class="slider-banner-slide__img_bg"></div><img src="assets/images/png/main-page/banner-sport.png" alt="banner-spa">
+        </div>
+      </div>
+    </div>
+    <div class="wrapper">
+      <div class="slider-banner-inner">
+        <div class="slider-banner__toggles">
+          <div class="js--swiper-pagination swiper-pagination"></div>
+          <div class="js--swiper-button-next swiper-button-next"></div>
+          <div class="js--swiper-button-prev swiper-button-prev"></div>
+        </div>
+      </div>
+    </div>
+    <div class="slider-banner__link-show-more"><a class="page-link page-link_arrow" href="/">Больше программ<div class="svg-cheveron-right svg-cheveron-right-box"></div></a></div>
+  </section>
+  <section class="revies">
+    <div class="wrapper">
+      <div class="revies-inner">
+        <from class="page-form">
+          <h3 class="page__title page_regular page__title-h3">Подпишитесь и будьте в курсе наших акций
+            и спецпредложений</h3><input class="page-input" type="tel" placeholder="Тедефон"><button class="page__btn page_broun-outline">Подписаться</button>
+        </from>
+      </div>
+    </div>
+  </section>
+</main>
+<?php get_footer(); ?>
